743c987d79f7b95fcacee4bf5d05d5144c5e1c75
Brollan <patrikbrolin@hotmail.com>
2022-02-15 10:06:04 +0100

    adding json file

--- script/main.js
+++ script/main.js
@@ -4,14 +4,14 @@
 const navLinks = document.querySelector(".navlinks")
 const hamburger = document.querySelector(".navbar")
 
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("hide")
-})
-
 // toggle shopping cart
-
 const shoppingCartIcon = document.querySelector(".fa-shopping-cart")
 const shoppingCart = document.querySelector(".shopping-cart-popup")
+
+hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("hide") 
+})
+
 
 shoppingCartIcon.addEventListener("click", () => {
     shoppingCart.classList.toggle("hide")
