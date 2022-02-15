5e4655dd55c32bbf8c8252a69b520a4777cf6474
Brollan <patrikbrolin@hotmail.com>
2022-02-15 09:55:49 +0100

    added shopingcart dropdown and footer design

--- script/main.js
+++ script/main.js
@@ -1,7 +1,18 @@
 
+// toggle hamburger
+
 const navLinks = document.querySelector(".navlinks")
 const hamburger = document.querySelector(".navbar")
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("hide")
+})
+
+// toggle shopping cart
+
+const shoppingCartIcon = document.querySelector(".fa-shopping-cart")
+const shoppingCart = document.querySelector(".shopping-cart-popup")
+
+shoppingCartIcon.addEventListener("click", () => {
+    shoppingCart.classList.toggle("hide")
 })
