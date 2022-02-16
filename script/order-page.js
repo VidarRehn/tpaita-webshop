8c734c99e2d69561b2996b4aa1a980a3dd93a82b
mickeProos <micke.roos.wahlbeck@gmail.com>
2022-02-16 17:18:56 +0100

    hej

--- script/order-page.js
+++ script/order-page.js
@@ -1,16 +1,16 @@
 function validation() {
   const inputfield1 = document.querySelectorAll(".input1").required;
   const inputfield2 = document.querySelectorAll(".input2").required;
-  const form = document.querySelectorAll(".order-form");
   document.getElementById("demo").innerHTML = inputfield1 + inputfield2;
   
-
+  
   
   const paybutton = document.querySelector(".pay-button")
   paybutton.addEventListener ("click", () => {
-  validation ()
+    validation ()
   });
 }
 
-form.addEventListener('submit', (e) => { 
-  e.preventDefault();
+//const form = document.querySelectorAll(".order-form");
+//form.addEventListener('submit', (e) => { 
+  //e.preventDefault();
