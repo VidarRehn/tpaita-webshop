5593d288b3140371a57b36d5121234c6e9d81518
Vidar Rehn <vidar.rehn@gmail.com>
2022-02-16 09:03:40 +0100

    separated js-file in product-list

--- script/main.js
+++ script/main.js
@@ -62,28 +62,3 @@ getProducts().then(data => {
     stringifyCategories(data.products)
 })
 
-
-// fixing breadcrumbs
-
-const breadcrumbCategory = document.querySelector("#breadcrumb-category")
-
-const fillingBreadcrumbs = (jsondata) => {
-    const params = new URLSearchParams(location.search)
-    const category = params.get("category")
-
-    if (category) {
-        console.log(category);
-        const currentCategory = jsondata.find(cat => {
-            console.log(cat)
-            return cat.category == category;
-        })
-        
-        breadcrumbCategory.innerText = currentCategory.category
-    }
-}
-
-getProducts().then(data => {
-    fillingBreadcrumbs(data.products)
-})
-
-
