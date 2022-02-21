690e03aa97975bb787dc3a000aa8c5f60ad48807
Vidar Rehn <vidar.rehn@gmail.com>
2022-02-21 15:05:30 +0100

    added fucntion for prepopulating fields in order page if logged in user

--- script/order-page.js
+++ script/order-page.js
@@ -11,3 +11,33 @@ form.addEventListener('submit', (e) => {
   window.location.href = "/tpaita-webshop/receipt.html"
 }
 )
+
+const firstNameInput = document.querySelector("#firstname")
+const lastNameInput = document.querySelector("#lastname")
+const emailInput = document.querySelector("#email")
+const telephoneInput = document.querySelector("#telephone")
+const addressInput = document.querySelector("#address-input")
+const zipCodeInput = document.querySelector("#zip")
+
+function prePopulateFields(){
+
+
+
+  let indexOfUserLoggedIn = userArray.findIndex((object) => {
+    return object.loggedin == true;
+  });
+
+  let currentUser = userArray[indexOfUserLoggedIn]
+
+  if (currentUser.name != "guest"){
+    let nameArray = currentUser.name.split(" ")
+    firstNameInput.value = nameArray[0]
+    lastNameInput.value = nameArray[1]
+    emailInput.value = currentUser.email
+    telephoneInput.value = currentUser.phone
+    addressInput.value = currentUser.address
+    zipCodeInput.value = currentUser.zipCode
+  }
+}
+
+prePopulateFields()
