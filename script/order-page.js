ac226b14dd190704c19e176d6e83b049e8d3118e
mickeProos <micke.roos.wahlbeck@gmail.com>
2022-02-21 09:42:19 +0100

    orderpage done

--- script/order-page.js
+++ script/order-page.js
@@ -2,15 +2,12 @@ function validation() {
   const inputfield1 = document.querySelectorAll(".input1").required;
   const inputfield2 = document.querySelectorAll(".input2").required;
   document.getElementById("demo").innerHTML = inputfield1 + inputfield2;
-  
-  
-  
-  const paybutton = document.querySelector(".pay-button")
-  paybutton.addEventListener ("click", () => {
-    validation ()
-  });
 }
 
-//const form = document.querySelectorAll(".order-form");
-//form.addEventListener('submit', (e) => { 
-  //e.preventDefault();
+const form = document.forms[2]
+form.addEventListener('submit', (e) => { 
+  e.preventDefault();
+  console.log("byter sida")
+  window.location.href = "/tpaita-webshop/receipt.html"
+}
+)
