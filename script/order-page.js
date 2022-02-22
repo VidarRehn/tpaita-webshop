70100299cb072f2d25b6ada62cda298af60ee389
mickeProos <micke.roos.wahlbeck@gmail.com>
2022-02-22 10:24:54 +0100

    fixed popup 4 orderpage

--- script/order-page.js
+++ script/order-page.js
@@ -5,12 +5,26 @@ function validation() {
 }
 
 const form = document.forms[2]
+const callButton = document.querySelector('.pay-button');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.close-button');
 form.addEventListener('submit', (e) => { 
   e.preventDefault();
   console.log("byter sida")
-  window.location.href = "/tpaita-webshop/receipt.html"
+  popupToggle = () => {
+    popup.classList.toggle('popup-opened');
+  }
+  
+  callButton.addEventListener('click', popupToggle);
+  
+  
+  
+  closeButton.addEventListener('click', (e) => { 
+    e.preventDefault();
+    window.location.href = "index.html"
+ 
 }
-)
+)});
 
 const firstNameInput = document.querySelector("#firstname")
 const lastNameInput = document.querySelector("#lastname")
